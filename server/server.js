def1fe12cb875300a50ce722c43b35f51bf99ee7
HenriquePRA <henriquep.r@hotmail.com>
2021-05-27 02:27:49 -0300

    atualizacao da rota principal

--- server/server.js
+++ server/server.js
@@ -8,3 +8,19 @@ const db = require("./db");
 // middleware //
 app.use(cors())
 app.use(express.json())
+
+// cadastro POST
+app.use("/cadastro", require("./routes/cadastro.js"))
+
+// login POST
+app.use("/login", require("./routes/login.js"))
+
+// consultas GET (nao eh obrigatorio auth)
+app.use("/consulta", require("./routes/consultas.js"))
+
+
+// execução do servidor
+const port = process.env.PORT || 5555
+app.listen(port, ()=> {
+    console.log(`Servidor sisclin online na porta ${port}`);
+});
